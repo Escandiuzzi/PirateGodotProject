b6e5fe1afda49371744fbfbb046c4d5ac5d9f3e4
Luiz Felipe Escandiuzzi <mrescandiuzzi@gmail.com>
2019-03-25 23:07:27 -0300

    Added pirate creation

---
+++ Pirate.gd
@@ -0,0 +1,35 @@
+extends Node2D
+
+var special = [];
+var id;
+
+# class member variables go here, for example:
+# var a = 2
+# var b = "textvar"
+
+func _ready():
+	for i in range(3):
+		special.append(null);
+	_initializePirate();
+	pass
+
+func _initializePirate():
+	print("New Pirate created with: ")
+	for i in range(3):
+		
+		var r = randi() & 100; 
+		if r  > 87: #rare pirate
+			var s = randi() % 9 + 1;
+			print(s);
+			special[i] = s;	
+		else: #common pirate
+			var s = randi() % 4;
+			print(s);
+			special[i] = s;	
+	
+	pass
+
+#func _process(delta):
+#	# Called every frame. Delta is time since last frame.
+#	# Update game logic here.
+#	pass
--- Player.gd
+++ Player.gd
@@ -6,42 +6,67 @@ export(int) var MOVEMENT_SPEED;
 
 # How close the sidekick must be to a point in the
 # path before moving on to the next one
-const POINT_RADIUS = 5
+const POINT_RADIUS = 5;
 
 # Path that the sidekick must follow - undefined by default
-var path
+var path;
+var crewCount = 0;
+var crew = [];
+
+
+onready var pirateObj = preload("res://Pirate.tscn");
 
 func _ready():
-	# Called when the node is added to the scene for the first time.
-	# Initialization here
+	for i in range(10):
+		crew.append(null);
 	pass
+
+func _input(ev):
+	if ev is InputEventKey and ev.scancode == KEY_K and not ev.echo and crewCount < 10:
+		_recruitPirate();
+	pass;
+
+func _recruitPirate():
+	var newPirate = pirateObj.instance();
+	var newPos = Vector2(position.x - 33, position.y + (crewCount * 54));
+	newPirate.position = newPos;
+	crew[crewCount] = newPirate;
+	crewCount += 1;
+	add_child(newPirate);
+	pass;
 
 # Performed on each step
 func _process(delta):
-
+	
 	# Only do stuff if we have a current path
 	if path:
 
 		# The next point is the first member of the path array
-		var target = path[0]
+		var target = path[0];
 
 		# Determine direction in which sidekick must move
-		var direction = (target - position).normalized()
+		var direction = (target - position).normalized();
 
 		# Move sidekick
-		position += direction * MOVEMENT_SPEED * delta
+		position += direction * MOVEMENT_SPEED * delta;
 
 		# If we have reached the point...
 		if position.distance_to(target) < POINT_RADIUS:
 
 			# Remove first path point
-			path.remove(0)
+			path.remove(0);
 
 			# If we have no points left, remove path
 			if path.size() == 0:
-				path = null
+				path = null;
 
-	pass
+	pass;
+
+func _saveData():
+	var save_nodes = get_tree().get_nodes_in_group("Persist");
+	for i in save_nodes:
+		var a;
+	pass;
 
 
 #func _process(delta):
